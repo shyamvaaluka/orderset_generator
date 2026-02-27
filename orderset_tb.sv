02ca5d8f21ee62dc8bb6849c4f763745b95ecd84
Reddy <undisree@xcoundisree50x.amd.com>
2026-02-27 13:03:46 +0530

    feb_27_13:03_pm

--- orderset_tb.sv
+++ orderset_tb.sv
@@ -8,7 +8,7 @@ module orderset_top;
   wire[31:0]cnt[0:15];
   wire[31:0]ts1_cnt[0:15];
   wire[31:0]ts2_cnt[0:15];
-
+  int fd1,fd2,l,k;
   orderset DUT(.clk(clk),.rst(rst),.command(command),.pipe_tx_data(pipe_tx_data),.cnt(cnt),.ts1_cnt(ts1_cnt),.ts2_cnt(ts2_cnt),.ltssm_states_h(ltssm_states_h));
 
   always #5 clk=~clk;
@@ -19,6 +19,8 @@ module orderset_top;
   endtask
 
   initial begin
+  fd1=$fopen("./ts1_done.txt","w+");
+  fd2=$fopen("./ts2_done.txt","w+");
     rst=1;
     for(int i=0;i<=15;i++)begin
     ltssm_states_h[i]=0;
@@ -37,39 +39,43 @@ module orderset_top;
     end
     command=0;
     fork
-    begin
-    while(1)begin
-    @(posedge clk);
-    for(int j=0;j<=15;j++)begin
-    if(ts1_cnt[j] == 12)begin
-     @(negedge clk);
-     for(int i=0;i<=15;i++)begin
-      ltssm_states_h[i]=2;
-     end
-     command=1;
-     break;
-    end
-    end
-    end
-    while(1) begin
-      @(posedge clk);
-      for(int j=0;j<=15;j++)begin
-      if(ts2_cnt[j] == 12)begin
-       @(negedge clk);
-       for(int i=0;i<=15;i++)begin
-        ltssm_states_h[i]=3;
-       end
-        #300;
-        $finish;
+      begin
+      while(1)begin
+        @(posedge clk);
+	  $fdisplay(fd1,"============count_ts1%0d==============",l++);
+        for(int j=0;j<=15;j++)begin
+          if(ts1_cnt[j] == 12)begin
+	   $fdisplay(fd1,$sformatf("ts1 count done at time=%0t",$time));
+           @(negedge clk);
+           for(int i=0;i<=15;i++)begin
+            ltssm_states_h[i]=2;
+           end
+           command=1;
+           break;
+          end
+        end
+      end
+      while(1) begin
+        @(posedge clk);
+	  $fdisplay(fd2,"============count_ts2%0d==============",k++);
+        for(int j=0;j<=15;j++)begin
+          if(ts2_cnt[j] == 12)begin
+	   $fdisplay(fd2,$sformatf("ts2 count done at time=%0t",$time));
+           @(negedge clk);
+           for(int i=0;i<=15;i++)begin
+            ltssm_states_h[i]=3;
+           end
+            #300;
+            break;
+          end
+        end
       end
       end
-    end
-    end
 
 
-    begin
-    #10000000;
-    end
+      begin
+      #10000000;
+      end
     join_any
     $finish;
   end 
