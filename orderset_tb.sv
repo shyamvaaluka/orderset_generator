e91498f980621be428761c6f55a7aeebf431758d
Reddy <undisree@xcoundisree50x.amd.com>
2026-02-27 14:52:22 +0530

    feb_27_14_52_pm

--- orderset_tb.sv
+++ orderset_tb.sv
@@ -8,7 +8,7 @@ module orderset_top;
   wire[31:0]cnt[0:15];
   wire[31:0]ts1_cnt[0:15];
   wire[31:0]ts2_cnt[0:15];
-  int fd1,fd2,l,k;
+  int flag;
   orderset DUT(.clk(clk),.rst(rst),.command(command),.pipe_tx_data(pipe_tx_data),.cnt(cnt),.ts1_cnt(ts1_cnt),.ts2_cnt(ts2_cnt),.ltssm_states_h(ltssm_states_h));
 
   always #5 clk=~clk;
@@ -19,8 +19,6 @@ module orderset_top;
   endtask
 
   initial begin
-  fd1=$fopen("./ts1_done.txt","w+");
-  fd2=$fopen("./ts2_done.txt","w+");
     rst=1;
     for(int i=0;i<=15;i++)begin
     ltssm_states_h[i]=0;
@@ -39,39 +37,45 @@ module orderset_top;
     end
     command=0;
     fork
-      begin
+     begin
       while(1)begin
         @(posedge clk);
-	  $fdisplay(fd1,"============count_ts1%0d==============",l++);
         for(int j=0;j<=15;j++)begin
           if(ts1_cnt[j] == 12)begin
-	   $fdisplay(fd1,$sformatf("ts1 count done at time=%0t",$time));
            @(negedge clk);
            for(int i=0;i<=15;i++)begin
             ltssm_states_h[i]=2;
            end
            command=1;
-           break;
           end
         end
+	for(int k=0;k<=15;k++)begin
+          if(ts1_cnt[k]==12)
+	    flag=1;
+	end
+	if(flag==1)
+	  break;
       end
       while(1) begin
         @(posedge clk);
-	  $fdisplay(fd2,"============count_ts2%0d==============",k++);
         for(int j=0;j<=15;j++)begin
           if(ts2_cnt[j] == 12)begin
-	   $fdisplay(fd2,$sformatf("ts2 count done at time=%0t",$time));
            @(negedge clk);
            for(int i=0;i<=15;i++)begin
             ltssm_states_h[i]=3;
            end
-            #300;
-            break;
+	    command=2;
+            repeat(10)
+	    @(negedge clk);
+            for(int j=0;j<=15;j++)
+	      ltssm_states_h[j]=0;
+	    repeat(3)
+	    @(negedge clk);
+            $finish;
           end
         end
       end
       end
-
 
       begin
       #10000000;
